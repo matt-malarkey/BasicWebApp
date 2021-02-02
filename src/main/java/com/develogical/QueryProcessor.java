1da1a643dd3bc00cf52c01ff545abe45c71bd99b
Matthew Malarkey <mm5917@ic.ac.uk>
2021-02-02 13:57:26 +0000

    Refactor string builder

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,28 +7,31 @@ public class QueryProcessor {
 
 
     public String process(String query) {
-        StringBuilder q = new StringBuilder(query.toLowerCase());
-        if (q.toString().contains(":")) {
-            String[] qs = q.toString().split(":");
-            q = new StringBuilder();
+        String q = query.toLowerCase();
+
+        // Get rid of query ID
+        StringBuilder sb = new StringBuilder();
+        if (q.contains(":")) {
+            String[] qs = q.split(":");
             for (int i = 1; i < qs.length; i++) {
-                q.append(qs[i]);
+                sb.append(qs[i]);
             }
         }
+        q = sb.toString();
 
-        if (q.toString().contains("shakespeare")) {
+        if (q.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
 
-        if (q.toString().contains("name")) {
+        if (q.contains("name")) {
             return "horse-battery-66";
         }
 
-        if (q.toString().contains("is the largest")) {
+        if (q.contains("is the largest")) {
             Pattern p = Pattern.compile("\\d+");
-            Matcher m = p.matcher(q.toString());
+            Matcher m = p.matcher(q);
             int largest = 0;
             while (m.find()) {
                 int i = Integer.parseInt(m.group());
@@ -41,7 +44,7 @@ public class QueryProcessor {
 
         if (query.contains("plus")) {
             Pattern p = Pattern.compile("\\d+");
-            Matcher m = p.matcher(q.toString());
+            Matcher m = p.matcher(q);
             int sum = 0;
             while (m.find()) {
                 int i = Integer.parseInt(m.group());
@@ -50,6 +53,6 @@ public class QueryProcessor {
             return Integer.toString(sum);
         }
 
-        return q.toString();
+        return q;
     }
 }
