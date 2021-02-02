69021cfe1e66a41c67a0d2026fdf0fff95626ff3
Matthew Malarkey <mm5917@ic.ac.uk>
2021-02-02 13:53:13 +0000

    Ignore query ID

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,21 +7,28 @@ public class QueryProcessor {
 
 
     public String process(String query) {
-        final String q = query.toLowerCase();
+        StringBuilder q = new StringBuilder(query.toLowerCase());
+        if (q.toString().contains(":")) {
+            String[] qs = q.toString().split(":");
+            q = new StringBuilder();
+            for (int i = 1; i < qs.length; i++) {
+                q.append(qs[i]);
+            }
+        }
 
-        if (q.contains("shakespeare")) {
+        if (q.toString().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
 
-        if (q.contains("name")) {
+        if (q.toString().contains("name")) {
             return "horse-battery-66";
         }
 
-        if (q.contains("is the largest")) {
+        if (q.toString().contains("is the largest")) {
             Pattern p = Pattern.compile("\\d+");
-            Matcher m = p.matcher(q);
+            Matcher m = p.matcher(q.toString());
             int largest = 0;
             while (m.find()) {
                 int i = Integer.parseInt(m.group());
@@ -30,18 +37,19 @@ public class QueryProcessor {
                 }
             }
             return Integer.toString(largest);
-        } else if (query.contains("plus")) {
+        }
+
+        if (query.contains("plus")) {
             Pattern p = Pattern.compile("\\d+");
-            Matcher m = p.matcher(q);
+            Matcher m = p.matcher(q.toString());
             int sum = 0;
             while (m.find()) {
                 int i = Integer.parseInt(m.group());
                 sum += i;
             }
-
             return Integer.toString(sum);
         }
 
-        return q;
+        return q.toString();
     }
 }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -28,6 +28,13 @@ public class QueryProcessorTest {
 
     @Test
     public void largestInt() {
-        assertThat(queryProcessor.process("which of the following numbers is the largest: 47, 5, 9, 657"), containsString("657"));
+        assertThat(queryProcessor.process("67fcfb30: which of the following numbers is the largest: 47, 5, 9, 657"), containsString("657"));
+        assertThat(queryProcessor.process("9999: which of the following numbers is the largest: 641, 18"), containsString("641"));
+    }
+
+    @Test
+    public void plus() {
+        // api?q=5076e740:%20what%20is%2010%20plus%2014
+        assertThat(queryProcessor.process("448r23e239: what is 10 plus 14"), containsString("24"));
     }
 }
